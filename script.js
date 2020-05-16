c24f50d110960ce18c7eefffa0376926888cf8f2
Wojtek <4golda4@gmail.com>
2020-05-17 00:35:54 +0200

    skonczenie projektu

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ var api;
 var procent;
 var procent2;
 var procent3;
+var pobranie;
+var kiedypobrane;
 
 
 function othername() {
     ilosc = document.getElementById("ilosc").value;
     zjakiej = document.getElementById("zjakiej").value;
     najaka = document.getElementById("najaka").value;
+    kiedypobrane = document.querySelector("div.wynik p+p+p");
 
     //  21 dni wstecz
     datebefore = new Date();
@@ -37,6 +40,8 @@ function othername() {
     //  console.log(ilosc + " ilosc\n" + zjakiej + "zjakiej\n" + najaka + "najaka\n" + today);
     if (najaka && ilosc && zjakiej) {
         download(datebefore, today);
+    } else {
+        alert("Wpisz i zaznacz wszystkie potrzebne dane!")
     }
 
 }
@@ -44,119 +49,123 @@ function othername() {
 async function download(datebefore, today) {
 
     // console.log(ilosc + " ilosc\n" + zjakiej + "zjakiej\n" + najaka + "najaka\n" + datebefore + "\n" + today);
-    api = "https://api.exchangeratesapi.io/history?start_at=" + datebefore + "&end_at=" + today + "&base=" + zjakiej;
+    api = "https://api.exchangeratesapi.io/latest?base=" + zjakiej;
     const response = await fetch(api);
 
     const data = await response.json();
+    pobranie = data.date;
+    console.log(pobranie);
+    kiedypobrane.textContent = "Day converter " + pobranie + " about the course:"
+
     // console.log(Object.values(data.rates)[0]);
     //  console.log(Object.keys(data.rates)[0]);
     switch (najaka) {
         case "CAD":
-            procent = Object.values(data.rates)[0].CAD;
-            procent2 = Object.values(data.rates)[1].CAD;
+            procent = data.rates.CAD;
+
             break;
         case "HKD":
-            procent = Object.values(data.rates)[0].HKD;
-            procent2 = Object.values(data.rates)[1].HKD;
+            procent = data.rates.HKD;
+
             break;
         case "ISK":
-            procent = Object.values(data.rates)[0].ISK;
+            procent = data.rates.ISK;
             break;
         case "PHP":
-            procent = Object.values(data.rates)[0].PHP;
+            procent = data.rates.PHP;
             break;
         case "DKK":
-            procent = Object.values(data.rates)[0].DKK;
+            procent = data.rates.DKK;
             break;
         case "HUF":
-            procent = Object.values(data.rates)[0].HUF;
+            procent = data.rates.HUF;
             break;
         case "CZK":
-            procent = Object.values(data.rates)[0].CZK;
+            procent = data.rates.CZK;
             break;
         case "GBP":
-            procent = Object.values(data.rates)[0].GBP;
+            procent = data.rates.GBP;
             break;
         case "RON":
-            procent = Object.values(data.rates)[0].RON;
+            procent = data.rates.RON;
             break;
         case "SEK":
-            procent = Object.values(data.rates)[0].SEK;
+            procent = data.rates.SEK;
             break;
         case "IDR":
-            procent = Object.values(data.rates)[0].IDR;
+            procent = data.rates.IDR;
             break;
         case "INR":
-            procent = Object.values(data.rates)[0].INR;
+            procent = data.rates.INR;
             break;
         case "BRL":
-            procent = Object.values(data.rates)[0].BRL;
+            procent = data.rates.BRL;
             break;
         case "RUB":
-            procent = Object.values(data.rates)[0].RUB;
+            procent = data.rates.RUB;
             break;
         case "HRK":
-            procent = Object.values(data.rates)[0].HRK;
+            procent = data.rates.HRK;
             break;
         case "JPY":
-            procent = Object.values(data.rates)[0].JPY;
+            procent = data.rates.JPY;
             break;
         case "THB":
-            procent = Object.values(data.rates)[0].THB;
+            procent = data.rates.THB;
             break;
         case "CHF":
-            procent = Object.values(data.rates)[0].CHF;
+            procent = data.rates.CHF;
             break;
         case "EUR":
-            procent = Object.values(data.rates)[0].EUR;
+            procent = data.rates.EUR;
             break;
         case "MYR":
-            procent = Object.values(data.rates)[0].MYR;
+            procent = data.rates.MYR;
             break;
         case "BGN":
-            procent = Object.values(data.rates)[0].BGN;
+            procent = data.rates.BGN;
             break;
         case "TRY":
-            procent = Object.values(data.rates)[0].TRY;
+            procent = data.rates.TRY;
             break;
         case "CNY":
-            procent = Object.values(data.rates)[0].CNY;
+            procent = data.rates.CNY;
             break;
         case "NOK":
-            procent = Object.values(data.rates)[0].NOK;
+            procent = data.rates.NOK;
             break;
         case "NZD":
-            procent = Object.values(data.rates)[0].NZD;
+            procent = data.rates.NZD;
             break;
         case "ZAR":
-            procent = Object.values(data.rates)[0].ZAR;
+            procent = data.rates.ZAR;
             break;
         case "USD":
-            procent = Object.values(data.rates)[0].USD;
+            procent = data.rates.USD;
             break;
         case "MXN":
-            procent = Object.values(data.rates)[0].MXN;
+            procent = data.rates.MXN;
             break;
         case "SGD":
-            procent = Object.values(data.rates)[0].SGD;
+            procent = data.rates.SGD;
             break;
         case "AUD":
-            procent = Object.values(data.rates)[0].AUD;
+            procent = data.rates.AUD;
             break;
         case "ILS":
-            procent = Object.values(data.rates)[0].ILS;
+            procent = data.rates.ILS;
             break;
         case "KRW":
-            procent = Object.values(data.rates)[0].KRW;
+            procent = data.rates.KRW;
             break;
         case "PLN":
-            procent = Object.values(data.rates)[0].PLN;
+            procent = data.rates.PLN;
             break;
 
     }
     document.querySelector("p.zwrot").textContent = (procent * ilosc).toFixed(2) + " " + najaka;
     document.querySelector("p.kurs").textContent = procent;
-    console.log(najaka);
+    //  console.log(najaka);
 
 
 }
